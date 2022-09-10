d9bbd785e25ea8dbb024a2db74d5473d2a01df2c
bdreiss <bd_reiss@yahoo.de>
2022-09-10 20:10:23 +0200

    Test

--- app/src/main/java/com/bdreiss/zentodo/MainActivity.java
+++ app/src/main/java/com/bdreiss/zentodo/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
+        //Test
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
