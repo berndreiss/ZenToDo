00d0492fff918767a840f80c5c95234ae8fe827f
bdreiss <bd_reiss@yahoo.de>
2022-09-17 18:54:29 +0200

    Established DatePickerDialog

--- app/src/main/java/com/bdreiss/zentodo/MainActivity.java
+++ app/src/main/java/com/bdreiss/zentodo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bdreiss.zentodo;
 
 import android.app.ActivityManager;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 
@@ -24,6 +25,7 @@ import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -31,6 +33,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -105,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 String task = editText.getText().toString();
                 editText.setText("");
                 if (!task.equals("")) {
+                    openDatePickerDialog(context);
                     data.add(task, " ", data.getDate(), " ");
                     items.add(task);
                     adapter.notifyDataSetChanged();
@@ -114,6 +118,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void openDatePickerDialog(Context context) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day)
+            {
+
+            }
+        }, year, month, day);
+        datePickerDialog.show();
     }
 
     public void setListView(Context context){
