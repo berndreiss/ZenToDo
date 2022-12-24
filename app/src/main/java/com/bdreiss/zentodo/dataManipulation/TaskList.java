6bd9e7fb60208d0a2ef10bb5c03235ce5f4199f2
bdreiss <bd_reiss@yahoo.de>
2022-12-24 15:37:40 +0100

    Created own class for taskList

---
+++ app/src/main/java/com/bdreiss/zentodo/dataManipulation/TaskList.java
@@ -0,0 +1,15 @@
+package com.bdreiss.zentodo.dataManipulation;
+
+public class TaskList {
+
+    int positionCount;
+    String color;
+
+    public TaskList(int positionCount, String color){
+
+        this.positionCount = positionCount;
+        this.color = color;
+
+    }
+
+}
