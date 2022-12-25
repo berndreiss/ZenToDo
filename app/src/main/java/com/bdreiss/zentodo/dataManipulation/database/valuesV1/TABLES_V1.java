3a140b3e73d0e5492282bb842bcef2200cbe2cb9
bdreiss <bd_reiss@yahoo.de>
2022-12-25 13:45:46 +0100

    Cleaned up

--- app/src/main/java/com/bdreiss/zentodo/dataManipulation/database/valuesV1/TABLES_V1.java
+++ app/src/main/java/com/bdreiss/zentodo/dataManipulation/database/valuesV1/TABLES_V1.java
@@ -1,13 +1,14 @@
-package com.bdreiss.zentodo.dataManipulation.database;
+package com.bdreiss.zentodo.dataManipulation.database.valuesV1;
 
 import androidx.annotation.NonNull;
 
-public enum TABLES{
-    TABLE_ENTRIES("entries"),  TABLE_LISTS("lists");
+public enum TABLES_V1 {
+    TABLE_ENTRIES("entries"),
+    TABLE_LISTS("lists");
 
     private String name;
 
-    TABLES(String name){
+    TABLES_V1(String name){
         this.name = name;
     }
 
