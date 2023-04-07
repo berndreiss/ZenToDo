833d2f8a91f1a1c114fc060fdcf2e3a75570009b
bdreiss <bd_reiss@yahoo.de>
2023-04-07 17:42:56 +0200

    Set DatePickerDialog always to today

--- app/src/main/java/com/bdreiss/zentodo/adapters/listeners/SetDateListener.java
+++ app/src/main/java/com/bdreiss/zentodo/adapters/listeners/SetDateListener.java
@@ -39,22 +39,11 @@ public class SetDateListener extends BasicListener implements View.OnClickListen
         int entryMonth;
         int entryYear;
 
-        //if current date greater then 0 set value, otherwise set today
-        if(entryDate>0) {
 
-            //resolve format "yyyymmdd"
-            entryDay = entryDate%100;
-            entryMonth = ((entryDate%10000)-entryDay)/100-1;
-            entryYear = (entryDate-entryMonth*100-entryDay)/10000;
-        }
-        else {
-
-            //get todays date
-            Calendar c = Calendar.getInstance();
-            entryYear = c.get(Calendar.YEAR);
-            entryMonth = c.get(Calendar.MONTH);
-            entryDay = c.get(Calendar.DAY_OF_MONTH);
-        }
+        Calendar c = Calendar.getInstance();
+        entryYear = c.get(Calendar.YEAR);
+        entryMonth = c.get(Calendar.MONTH);
+        entryDay = c.get(Calendar.DAY_OF_MONTH);
 
         //create DatePickerDialog and set listener
         DatePickerDialog datePickerDialog = getDatePickerDialog(entry, entryDay,entryMonth,entryYear,holder,position);
